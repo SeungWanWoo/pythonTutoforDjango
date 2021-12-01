0f0582ce5dfdf16b60ae0d391eab0d9f4aceb808
SeungWanWoo <46276591+SeungWanWoo@users.noreply.github.com>
2021-12-01 17:12:39 +0900

    Delete return4.py

--- return4.py
+++
@@ -1,8 +0,0 @@
-def compound_interest_amount(p, r, t, n):
-    return p * (1 + r / n) ** (n * t)
-
-def __main__():
-    print(compound_interest_amount(1500000, 0.043, 6, 4))
-    print(compound_interest_amount(1500000, 0.043, 6, 1/2))
-
-__main__()
